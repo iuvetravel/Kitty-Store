d7521d53caabccb048f6883670487406a9744030
Calin Puscas <calinpuscas@gmail.com>
2021-04-28 10:56:03 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,4 +3,13 @@ $(document).ready(function () {
     $(this).toggleClass("fa-times");
     $(".navbar").toggleClass("nav-toggle");
   });
+  $(window).on("scroll load", function () {
+    $("#menu-bar").removeClass("fa-times");
+    $(".navbar").removeClass("nav-toggle");
+  });
+  $(".sleep .list .btn").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+    let src = $(this).attr("data-src");
+    $("#sleep-img").attr("src", src);
+  });
 });
