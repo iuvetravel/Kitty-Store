27bb90deb00b68d916a1212750dbf233d05f7b2b
Calin Puscas <calinpuscas@gmail.com>
2021-04-29 12:36:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,13 +3,27 @@ $(document).ready(function () {
     $(this).toggleClass("fa-times");
     $(".navbar").toggleClass("nav-toggle");
   });
-  $(window).on("scroll load", function () {
+
+  $(window).on("load scroll", function () {
     $("#menu-bar").removeClass("fa-times");
     $(".navbar").removeClass("nav-toggle");
+
+    $("section").each(function () {
+      let top = $(window).scrollTop();
+      let height = $(this).height();
+      let id = $(this).attr("id");
+      let offset = $(this).offset().top - 200;
+
+      if (top > offset && top < offset + height) {
+        $(".navbar ul li a").removeClass("active");
+        $(".navbar").find(`[href="#${id}"]`).addClass("active");
+      }
+    });
   });
-  $(".sleep .list .btn").click(function () {
+
+  $(".list .btn").click(function () {
     $(this).addClass("active").siblings().removeClass("active");
     let src = $(this).attr("data-src");
-    $("#sleep-img").attr("src", src);
+    $(".menu .row .image img").attr("src", src);
   });
 });
